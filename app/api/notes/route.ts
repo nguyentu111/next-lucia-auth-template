e2b85543af39d2c7326f6e72aed942fa627fe92f
nguyentu111 <64887106+nguyentu111@users.noreply.github.com>
2024-06-21 14:31:10 +0700

    u

--- app/api/notes/route.ts
+++ app/api/notes/route.ts
@@ -1,6 +1,8 @@
 import { db } from "@/lib/db";
 import { NextRequest } from "next/server";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 export async function GET(req: NextRequest) {
   return Response.json(await db.note.findMany());
 }
